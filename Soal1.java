6a7ca08dd21927be44a40732b5c4521bedd8c12f
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2020-01-07 16:44:02 +0700

    Membua File Pengulangan Soal1.java

---
+++ Soal1.java
@@ -0,0 +1,8 @@
+public class Soal1{
+    public static void main(String[] args){
+        for(int i=1;i<=100;i++){
+            int j = i*2;
+            System.out.println(i+" Kg "+" = "+j+" pon ");
+        }
+    }
+}
